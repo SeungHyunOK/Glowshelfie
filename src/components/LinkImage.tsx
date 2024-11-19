602a80e234883a62e0336f8bc4d22dc4587c54cb
OK <ohl2619@naver.com>
2024-11-19 22:02:33 +0900

    fix: add someTing

--- src/components/LinkImage.tsx
+++ src/components/LinkImage.tsx
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Image from 'next/image'
-
+import React, { useState, useEffect } from 'react'
 interface LinkImageProps {
   children: string
 }
