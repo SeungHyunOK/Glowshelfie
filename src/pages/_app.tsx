fb494aef6b172f2ffdeaee2b2d97015dbab06604
OK <ohl2619@naver.com>
2024-11-19 17:38:04 +0900

    fix: fix done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 import '@/styles/global.css'
-import Nav from '@/components/nav'
+import Nav from '@/components/Nav'
 import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
